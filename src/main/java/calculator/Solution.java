c16f906c8dbae7bac110730ab69177f608b3ba5c
RatmirA <ratmir7854@gmail.ru>
2020-11-19 11:23:53 -0500

    Add new

---
+++ src/main/java/calculator/Solution.java
@@ -0,0 +1,45 @@
+package calculator;
+
+import java.util.*;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
+
+public final class Solution {
+    private static final String ROMAN = "I?[XV]|V?II?I?";
+    private static final String ARABIC = "[1-9]|10";
+    private static final String OPERATION = "[-+*/]";
+    private static final String OR = "|";
+    private static final Map<String, IntBinaryOperator> calculator = Map.of(
+            "-", (x, y) -> x - y,
+            "+", (x, y) -> x + y,
+            "/", (x, y) -> x / y,
+            "*", (x, y) -> x * y);
+
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var a = scanner.next(ROMAN + OR + ARABIC);
+        final var op = scanner.next(OPERATION);
+        final var isRoman = a.matches(ROMAN);
+        final var b = scanner.next(isRoman ? ROMAN : ARABIC);
+
+        final ToIntFunction<String> toInt = isRoman ? Solution::romanToArabic : Integer::parseInt;
+        final IntFunction<String> toString = isRoman ? Solution::arabicToRoman : Integer::toString;
+
+        final int result = calculator.get(op).applyAsInt(toInt.applyAsInt(a), toInt.applyAsInt(b));
+
+        System.out.println(toString.apply(result));
+    }
+
+    static int romanToArabic(final String number) {
+        return 1 + List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X").indexOf(number);
+    }
+
+    static String arabicToRoman(final int number) {
+        return String.join("",
+                number == 100 ? "C" : "",
+                new String[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}[number % 100 / 10],
+                new String[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}[number % 10]
+        );
+    }
+}
---
+++ src/test/java/calculator/Test.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/romanToArabic.csv", delimiter = '=')
+    void romanToArabic(String input, int expected) {
+        assertEquals(expected, Solution.romanToArabic(input));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/arabicToRoman.csv", delimiter = '=')
+    void arabicToRoman(int input, String expected) {
+        assertEquals(expected, Solution.arabicToRoman(input));
+    }
+}
